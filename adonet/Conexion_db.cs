63de843e0e292b3b4a523360890399b248e3b3ec
Rositha Orellana <Rositha Orellana@LAPTOP-8K9OT5UR>
2020-04-05 18:42:10 -0600

    Agregar archivos de proyecto.

---
+++ adonet/Conexion_db.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient; 
+
+namespace adonet
+{
+    class Conexion_db
+    {
+        SqlConnection miConexion = new SqlConnection();
+        SqlCommand comandosSQL = new SqlCommand();
+        SqlDataAdapter miAdaptadorDatos = new SqlDataAdapter();
+
+        DataSet ds = new DataSet();
+
+        public Conexion_db()
+        {
+            String cadena = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\sistemadebicirepuestos.mdf;Integrated Security=True";
+            miConexion.ConnectionString = cadena;
+            miConexion.Open();
+        }
+        public DataSet obtener_datos()
+        {
+            ds.Clear();
+            comandosSQL.Connection = miConexion;
+
+            comandosSQL.CommandText = "select * from clientes";
+            miAdaptadorDatos.SelectCommand = comandosSQL;
+            miAdaptadorDatos.Fill(ds, "clientes");
+
+            comandosSQL.CommandText = "select * from productos";
+            miAdaptadorDatos.SelectCommand = comandosSQL;
+            miAdaptadorDatos.Fill(ds, "productos");
+
+            comandosSQL.CommandText = "select * from ventas";
+            miAdaptadorDatos.SelectCommand = comandosSQL;
+            miAdaptadorDatos.Fill(ds, "ventas");
+
+            comandosSQL.CommandText = "select * from dventas";
+            miAdaptadorDatos.SelectCommand = comandosSQL;
+            miAdaptadorDatos.Fill(ds, "dventas");
+
+            return ds;
+
+        }
+        public void mantenimiento_datos(String[] datos, String accion)
+        {
+            String sql = "";
+            if (accion == "nuevo")
+            {
+                sql = "INSERT INTO clientes (codigo,nombre,direccion,telefono,dui,nit) VALUES(" +
+                    "'" + datos[1] + "'," +
+                    "'" + datos[2] + "'," +
+                    "'" + datos[3] + "'," +
+                    "'" + datos[4] + "'," +
+                    "'" + datos[5] + "'," +
+                    "'" + datos[6] + "'" +
+                    ")";
+
+            }
+            else if (accion == "modificar")
+            {
+                sql = "UPDATE clientes SET " +
+                    "codigo         = '" + datos[1] + "'," +
+                    "nombre         = '" + datos[2] + "'," +
+                    "direccion      = '" + datos[3] + "'," +
+                    "telefono       = '" + datos[4] + "'," +
+                    "dui            = '" + datos[5] + "'," +
+                    "nit            = '" + datos[6] + "'" +
+                    "WHERE idCliente = '" + datos[0] + "'";
+            }
+            else if (accion == "eliminar")
+            {
+                sql = "DELETE clientes FROM clientes WHERE idCliente='" + datos[0] + "'";
+            }
+            procesarSQL(sql);
+        }
+        void procesarSQL(String sql)
+        {
+            comandosSQL.Connection = miConexion;
+            comandosSQL.CommandText = sql;
+            comandosSQL.ExecuteNonQuery(); 
+
+        }
+    }       
+}
---
+++ adonet/Form2.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace adonet
+{
+    partial class busqueda_clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.btnseleccionar = new System.Windows.Forms.Button();
+            this.grdBusquedaClientes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdBusquedaClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.Pink;
+            this.btncancelar.Location = new System.Drawing.Point(394, 230);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(75, 23);
+            this.btncancelar.TabIndex = 5;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.UseVisualStyleBackColor = false;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.BackColor = System.Drawing.Color.Pink;
+            this.txtbuscar.Location = new System.Drawing.Point(214, 230);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(100, 20);
+            this.txtbuscar.TabIndex = 6;
+            this.txtbuscar.TextChanged += new System.EventHandler(this.txtbuscar_TextChanged);
+            // 
+            // btnseleccionar
+            // 
+            this.btnseleccionar.BackColor = System.Drawing.Color.Pink;
+            this.btnseleccionar.Location = new System.Drawing.Point(39, 230);
+            this.btnseleccionar.Name = "btnseleccionar";
+            this.btnseleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnseleccionar.TabIndex = 4;
+            this.btnseleccionar.Text = "Seleccionar";
+            this.btnseleccionar.UseVisualStyleBackColor = false;
+            this.btnseleccionar.Click += new System.EventHandler(this.btnseleccionar_Click);
+            // 
+            // grdBusquedaClientes
+            // 
+            this.grdBusquedaClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdBusquedaClientes.Location = new System.Drawing.Point(12, 12);
+            this.grdBusquedaClientes.Name = "grdBusquedaClientes";
+            this.grdBusquedaClientes.Size = new System.Drawing.Size(496, 212);
+            this.grdBusquedaClientes.TabIndex = 7;
+            // 
+            // busqueda_clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(520, 265);
+            this.Controls.Add(this.grdBusquedaClientes);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.txtbuscar);
+            this.Controls.Add(this.btnseleccionar);
+            this.Name = "busqueda_clientes";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.busqueda_clientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdBusquedaClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Button btnseleccionar;
+        private System.Windows.Forms.DataGridView grdBusquedaClientes;
+    }
+}
